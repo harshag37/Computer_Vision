b5629fa71295479129cb9dd82aefc6279e7d084e
Harsh Agrawal <57638373+harshag37@users.noreply.github.com>
2021-08-24 19:49:09 +0530

    Update cascade_det.py

--- Cascade_detection/cascade_det.py
+++ Cascade_detection/cascade_det.py
@@ -5,7 +5,7 @@ vid=cv2.VideoCapture(0)
 while True:
     ret,img=vid.read()
     imggray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    sign=stopcascade.detectMultiScale(imggray,1.1,3)
+    sign=stopcascade.detectMultiScale(imggray,1.0,3)
     for (x,y,w,h) in sign:
         cv2.rectangle(img,(x,y),(x+w,y+h),(255,255,255),2)
     cv2.imshow("output",img)
